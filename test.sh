0cc8e9422492ea72bb3856acd388338067363522
Nicolás Ortega Froysa <nicolas@ortegas.org>
2025-12-11 08:06:37 +0100

    Add test to find last prime before 10^9.

--- test.sh
+++ test.sh
@@ -17,7 +17,7 @@ fi
 tests=0
 passed=0
 
-## TEST 1
+## TEST
 ((tests++))
 echo -n "${tests}: Find all prime numbers before 70..."
 expect="2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 "
@@ -30,7 +30,20 @@ else
 	echo " FAIL"
 fi
 
-## TEST 2
+## TEST
+((tests++))
+echo -n "${tests}: Find last prime before 1,000,000,000..."
+expect="999999937"
+
+if [ "$("$BINARY" 1000000000)" = "$expect" ]
+then
+	echo " pass"
+	((passed++))
+else
+	echo " FAIL"
+fi
+
+## TEST
 ((tests++))
 echo -n "${tests}: 11 is prime..."
 if "$BINARY" -t 11
@@ -41,7 +54,7 @@ else
 	echo " FAIL"
 fi
 
-## TEST 3
+## TEST
 ((tests++))
 echo -n "${tests}: 9 is not prime..."
 if ! "$BINARY" -t 9
@@ -54,3 +67,8 @@ fi
 
 ## RESULTS
 echo "Results: $passed/$tests"
+
+if [ $passed -ne $tests ]
+then
+	exit 1
+fi
